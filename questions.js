27f111c8188e0af457871a53c16c228a60166509
Baizkunthaz <baikunthabihari6@gmail.com>
2023-12-18 12:21:47 +0530

    Add existing project files prior to the push to GitHub.

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "HTML Stands for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is a Javascript Framework?",
+    answer: "Vue",
+    options: [
+      "C",
+      "Python",
+      "Vue",
+      "kotlin"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the common boundary present between two systems known as?",
+    answer: "Interface",
+    options: [
+      "Interface",
+      "Surface",
+      "Interdiction",
+      "None of the Above"
+    ]
+  }
+];
